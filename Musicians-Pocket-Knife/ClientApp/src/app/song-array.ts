e020e90ad4c59a412873b778bb0ca7c96171c9e7
Dan Schuler <104746354+dandanthesoftwareman@users.noreply.github.com>
2022-11-22 16:06:32 -0500

    created SongArray interface

---
+++ Musicians-Pocket-Knife/ClientApp/src/app/song-array.ts
@@ -0,0 +1,21 @@
+export interface SongArray {
+    search: Search[];
+}
+
+export interface Search {
+    id:     string;
+    title:  string;
+    uri:    string;
+    artist: Artist;
+}
+
+export interface Artist {
+    id:     string;
+    name:   string;
+    uri:    string;
+    img:    null | string;
+    genres: string[] | null;
+    from:   null | string;
+    mbid:   string;
+}
+
