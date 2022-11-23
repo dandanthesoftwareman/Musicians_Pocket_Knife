059225be130c8f2163ca551d3fc1eb54fb40ae8c
Dan Schuler <104746354+dandanthesoftwareman@users.noreply.github.com>
2022-11-23 12:59:18 -0500

    added user login variables to findsong component

--- Musicians-Pocket-Knife/ClientApp/src/app/findsong/findsong.component.ts
+++ Musicians-Pocket-Knife/ClientApp/src/app/findsong/findsong.component.ts
@@ -3,6 +3,8 @@ import { ApireturnsongarrayService } from '../apireturnsongarray.service';
 import { NgForm } from '@angular/forms';
 import { SongArray } from '../song-array';
 import { Song } from '../song';
+import { SocialAuthService, SocialUser } from '@abacritt/angularx-social-login';
+import { UserService } from '../user.service';
 
 
 @Component({
@@ -14,13 +16,20 @@ export class FindsongComponent implements OnInit {
 
   song:Song = {} as Song;
   songArray:SongArray = {} as SongArray;
-  
   displaySongInfo:boolean = false;
-  constructor(private apisongservice: ApireturnsongarrayService) { }
+
+  user: SocialUser = {} as SocialUser;
+  loggedIn: boolean = false;
+  
+  constructor(private apisongservice: ApireturnsongarrayService, private authService:SocialAuthService) { }
 
   ngOnInit(): void {
-
-  }
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      UserService.user.id = user.id;
+      this.loggedIn = (user != null);
+  })
+}
   
   //uses name=searched song of searchSongForm, trims and formats it to string for API call
   searchForSong(form:NgForm):SongArray{
