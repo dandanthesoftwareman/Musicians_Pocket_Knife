b5e579d667b919593e06791778d36694e4938b14
Dan Schuler <104746354+dandanthesoftwareman@users.noreply.github.com>
2023-01-05 10:30:21 -0500

    updated playlist details oninit

--- Musicians-Pocket-Knife/ClientApp/src/app/playlist-details/playlist-details.component.ts
+++ Musicians-Pocket-Knife/ClientApp/src/app/playlist-details/playlist-details.component.ts
@@ -1,6 +1,8 @@
+import { SocialAuthService, SocialUser } from '@abacritt/angularx-social-login';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { PlaylistService } from '../playlist.service';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-playlist-details',
@@ -9,9 +11,16 @@ import { PlaylistService } from '../playlist.service';
 })
 export class PlaylistDetailsComponent implements OnInit {
 
-  constructor(private http:HttpClient, private playlistService: PlaylistService) { }
+  constructor(private http:HttpClient, private authService:SocialAuthService, private playlistService: PlaylistService) { }
 
+  user: SocialUser = {} as SocialUser;
+  loggedIn: boolean = false;
+  
   ngOnInit(): void {
-  }
-
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      UserService.user.id = user.id;
+      this.loggedIn = (user != null);
+  })
+}
 }
