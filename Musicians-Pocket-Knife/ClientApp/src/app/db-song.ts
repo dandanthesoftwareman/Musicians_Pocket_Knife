f597eabefb012b35bedc627434fb49b5634c646b
Dan Schuler <104746354+dandanthesoftwareman@users.noreply.github.com>
2023-01-11 13:48:00 -0500

    Playlist Details now populates with song details

--- Musicians-Pocket-Knife/ClientApp/src/app/db-song.ts
+++ Musicians-Pocket-Knife/ClientApp/src/app/db-song.ts
@@ -1,11 +1,11 @@
 export interface DbSong {
-    Id:             number;
-    PlaylistId:     number;
-    Apiid:          string;
-    Title:          string;
-    Artist:         string;
-    Tempo:          string;
-    TimeSignature:  string;
-    OriginalKey:    string;
-    TransposedKey:  string;
+    id:             number;
+    playlistid:     number;
+    apiid:          string;
+    title:          string;
+    artist:         string;
+    tempo:          string;
+    timesignature:  string;
+    originalkey:    string;
+    transposedkey:  string;
 }
--- Musicians-Pocket-Knife/ClientApp/src/app/playlist-details/playlist-details.component.ts
+++ Musicians-Pocket-Knife/ClientApp/src/app/playlist-details/playlist-details.component.ts
@@ -26,13 +26,13 @@ export class PlaylistDetailsComponent implements OnInit {
       this.user = user;
       UserService.user.id = user.id;
       this.loggedIn = (user != null);
-      let params = this.route.snapshot.paramMap;
+  })
+  let params = this.route.snapshot.paramMap;
       this.listTitle = String(params.get("listTitle"));
       this.playlistService.ViewPlaylistDetails(this.listTitle).subscribe((response:DbSong[])=>{
         this.listSongs = response;
         console.log(this.listSongs);
       })
-  })
 }
 
 }
